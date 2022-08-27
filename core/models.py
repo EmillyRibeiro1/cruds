b1a76dcd4e2df9351c47ea51818d5d187dde53f6
Emilly <emillyribeirosouza123@hotmail.com>
2022-08-27 17:59:12 -0300

    adicionei campos genero,classificação, lançamentos

--- core/models.py
+++ core/models.py
@@ -1,4 +1,8 @@
+from __future__ import generator_stop
+from socket import NI_NUMERICHOST
 from django.db import models
 
-class cruds:
-    
+class cruds(models.Model):
+    generos = models.CharField ("Gênero", max_length=100)
+    classificação = models.IntegerField ("Classificação Indicativa")
+    Lançamentos = models.CharField ("Lançamentos", max_length=100) 
