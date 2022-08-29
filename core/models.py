85e77a04e19c1347fce48a74911b9b3b110301ca
Emilly <emillyribeirosouza123@hotmail.com>
2022-08-29 15:59:27 -0300

    adicionei novo campo

--- core/models.py
+++ core/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class cruds(models.Model):
+    nome = models.CharField ("Nome", max_length=100)
     generos = models.CharField ("Gênero", max_length=100)
     classificação = models.IntegerField ("Classificação Indicativa")
-    lançamentos = models.CharField ("Lançamentos", max_length=100) 
